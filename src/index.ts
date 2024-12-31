96b71fe583a2daba12836e73e3e5fc480ef3e0bc
medi_rudiant <medirudiantoni2@gmail.com>
2024-12-31 20:15:14 +0700

    tes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import route from "./routes/index.route";
 import cors from "cors"
 const app = express();
 // const port = 5000;
-const port = 3000;
+const port = 8080;
 
 app.use(express.json());
 app.use("/public", express.static("public"));
@@ -12,7 +12,7 @@ app.use(cors())
 app.use("/api", route);
 
 app.use('/', (req, res) => {
-    res.send("Hello world!")
+    res.send("Hello world! hehe")
 });
 
 
